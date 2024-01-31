7cd4c55feee30fe68f03c9be519e05aff33cc1fc
Angeles y Mixa <213110551@upmh.edu.mx>
2024-01-31 03:49:06 -0600

    agregue la primer mejora

--- Ahorcadojuegobase1.py
+++ Ahorcadojuegobase1.py
@@ -20,87 +20,82 @@ class AhorcadoGame:
 
     def dibujar_ahorcado(self):
         if self.intentos == 0:
-            print("  ____")
+            print("  __")
             print(" |    |")
             print(" |")
             print(" |")
             print(" |")
-            print(" |_________")
+            print(" |___")
         elif self.intentos == 1:
-            print("  ____")
+            print("  __")
             print(" |    |")
             print(" |    O")
             print(" |")
             print(" |")
-            print(" |_________")
+            print(" |___")
         elif self.intentos == 2:
-            print("  ____")
+            print("  __")
             print(" |    |")
             print(" |    O")
             print(" |    |")
             print(" |")
-            print(" |_________")
+            print(" |___")
         elif self.intentos == 3:
-            print("  ____")
+            print("  __")
             print(" |    |")
             print(" |    O")
             print(" |   /|")
             print(" |")
-            print(" |_________")
+            print(" |___")
         elif self.intentos == 4:
-            print("  ____")
+            print("  __")
             print(" |    |")
             print(" |    O")
             print(" |   /|\\")
             print(" |")
-            print(" |_________")
+            print(" |___")
         elif self.intentos == 5:
-            print("  ____")
+            print("  __")
             print(" |    |")
             print(" |    O")
             print(" |   /|\\")
             print(" |   /")
-            print(" |_________")
+            print(" |___")
         elif self.intentos == 6:
-            print("  ____")
+            print("  __")
             print(" |    |")
             print(" |    O")
             print(" |   /|\\")
             print(" |   / \\")
-            print(" |_________")
+            print(" |___")
 
     def jugar(self):
         print("¿Jugamos AhorcadoGame?")
-        print("Adivina la palabra oculta letra por letra ¡Suerte UwU!")
+        print("Adivina la palabra oculta letra por letra. ¡Suerte UwU!")
         print(self.mostrar_palabra())
 
         while True:
             letra = input("Adivina una letra: ")
-
-            if letra in self.letras_adivinadas:
-                print("Ya sabes que esa letra es correcta. Intenta con otra, vamos.")
-                continue
 
             self.letras_adivinadas.append(letra)
 
             if letra not in self.palabra_secreta:
                 self.intentos += 1
                 self.dibujar_ahorcado()
-                print("Letra incorrecta. Intentos restantes:", self.intentos_maximos - self.intentos)
+                print("Letra incorrecta,. Intentos restantes:", self.intentos_maximos - self.intentos)
             else:
-                print("whoosh, palabra correctaa")
+                print("¡whoosh, letra correcta!")
 
             palabra_mostrada = self.mostrar_palabra()
             print(palabra_mostrada)
 
             if palabra_mostrada.replace(" ", "") == self.palabra_secreta:
-                print("Que pro!, le has dado a la palabra.")
+                print("¡Que pro!, le has dado a la palabra.")
                 break
 
             if self.intentos == self.intentos_maximos:
                 print("Este no era tu momento, has agotado los intentos. Tu palabra secreta era:", self.palabra_secreta)
                 break
-
 
 if __name__ == "__main__":
     juego = AhorcadoGame()
